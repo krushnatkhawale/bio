5662d795ed02f7905d286f7c3e40ea69a74ff0dc
krushnatkhawale@gmail.com <krushnatkhawale@gmail.com>
2019-10-20 21:43:47 +0100

    Updates

---
+++ precache-manifest.7947640a7eb40bff4223eeff093bda63.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53973211d9be9284f0490d68847fd06e",
+    "url": "/bio/index.html"
+  },
+  {
+    "revision": "fd6cbeb107391fbe3e03",
+    "url": "/bio/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "2544bc1c83b3e6446dfb",
+    "url": "/bio/static/js/2.b1e97f99.chunk.js"
+  },
+  {
+    "revision": "fd6cbeb107391fbe3e03",
+    "url": "/bio/static/js/main.54de584d.chunk.js"
+  },
+  {
+    "revision": "e4e96e244cef190a7909",
+    "url": "/bio/static/js/runtime-main.bef51046.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/bio/static/media/logo.25bf045c.svg"
+  }
+]);
